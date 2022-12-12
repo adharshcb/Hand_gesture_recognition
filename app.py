81151d91b7c3f0cb59c701dcddbffc8926149be9
adharshcb <adharshcheloor98@gmail.com>
2022-12-12 11:55:14 +0530

    all characters added for training

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--width", help='cap width', type=int, default=1280)
-    parser.add_argument("--height", help='cap height', type=int, default=720)
+    parser.add_argument("--width", help='cap width', type=int, default=640)
+    parser.add_argument("--height", help='cap height', type=int, default=480)
 
     parser.add_argument('--use_static_image_mode', action='store_true')
     parser.add_argument("--min_detection_confidence",
